138f2acd2d38f4621e5a3db9271d25f37feefd60
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-05-15 22:34:57 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//Whack-A-Mole
+
+//target score and save in a variable.
+//target timer and save in a variable.
+//target the mole and save in a variable.
+
+//target all buttons and save in a variable.
+    //use a forEach() method
+    //add event listeners
+
+//intialize the results to zero and save in a variable.
+
+//create a function to get a random square.
+    //forEach() 
+        //remove the mole if it exists on any square. 
+    
+    //you need to pass a random number (0 to 8) into the squares array.
+    //get a random number and save in a variable.
+        //Math.random generated random number multiple by the length of the array.
+        //Math.floor rounds down the generated random number.
+    //assign the mole class to the random number. 
+
+
+//create a function for the mole moving.
+    //get the timerID and set it to null.
+    //use setInterval to set how quickly the mole will jump from square to square. 
+
+//you want to get a point every time you hit the mole
+    //forEach()
+    //addEventListener    mousedown() event
+    //if the square ID === the random square
+    //return score and add a point
+    //display result on screen
+
+//get the time to stop working when it gets to 0
+//create a variable to default the default time (60 seconds)
+//create a countdown function
+//default time - 1 second
+//display time left
+//if the time === 0 clear the interval
+//alert GAME OVER
+
+
+
